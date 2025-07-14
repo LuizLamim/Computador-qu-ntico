8f2fa4de1f43136db07c3aefaaa4acb5d7393d36
LuizLamim <luiz.britohenrique@hotmail.com>
2025-07-14 20:49:41 -0300

    estrelas

---
+++ estrelas.py
@@ -0,0 +1,14 @@
+estrelas = [
+    "UY Scuti",
+    "Stephenson 2-18",
+    "RW Cephei",
+    "WOH G64",
+    "Westerlund 1-26"
+]
+
+
+print("As 5 maiores estrelas conhecidas (em ordem decrescente de tamanho):")
+
+
+for estrela in estrelas:
+    print(estrela)
