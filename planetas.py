7f2dcba2447963f937f95269eac669d238a7646a
LuizLamim <luiz.britohenrique@hotmail.com>
2025-07-14 20:48:09 -0300

    Planetas

---
+++ planetas.py
@@ -0,0 +1,18 @@
+# Lista dos planetas em ordem a partir do Sol
+planetas = [
+    "Mercúrio",
+    "Vênus",
+    "Terra",
+    "Marte",
+    "Júpiter",
+    "Saturno",
+    "Urano",
+    "Netuno"
+]
+
+# Imprime a lista dos planetas
+print("Planetas do Sistema Solar em ordem a partir do Sol:")
+
+# Usa um loop 'for' para percorrer e imprimir cada planeta na lista
+for planeta in planetas:
+    print(planeta)
