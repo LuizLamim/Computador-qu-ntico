d655893ebfe4e8c1c675bd9a01db9e97332c5d98
LuizLamim <luiz.britohenrique@hotmail.com>
2025-07-17 22:06:14 -0300

    vector

---
+++ vector.py
@@ -0,0 +1,11 @@
+import numpy as np # Importa a biblioteca NumPy e a apelida de 'np'
+
+# Criação dos vetores unitários usando np.array
+# Vetor unitário na direção X
+vetor1 = np.array([1.0, 0.0, 0.0]) 
+# Vetor unitário na direção Y
+vetor2 = np.array([0.0, 1.0, 0.0])
+
+# Exibe os vetores
+print("Primeiro vetor unitário:", vetor1)
+print("Segundo vetor unitário:", vetor2)
