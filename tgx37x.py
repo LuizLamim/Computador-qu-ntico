90ecb257a0d8974b9a230af0c3f78b0bd95172d3
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-05 19:43:49 -0300

    esse

---
+++ tgx37x.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+  """
+  Calcula o valor da função f(x) = tan(x) + 37x.
+  """
+  return np.tan(x) + 37 * x
+
+# Cria um intervalo de valores para x.
+# O intervalo foi escolhido para evitar as descontinuidades da tangente (pi/2, 3pi/2, etc.).
+# Você pode ajustar o intervalo se precisar de uma visualização diferente.
+x = np.linspace(-3 * np.pi, 3 * np.pi, 1000)
+
+# Calcula os valores de y para cada valor de x.
+y = f(x)
+
+# Plota o gráfico.
+plt.figure(figsize=(10, 6))  # Define o tamanho da figura
+plt.plot(x, y, label='tan(x) + 37x', color='blue')
+
+# Adiciona título e rótulos aos eixos
+plt.title('Gráfico da função $f(x) = \\tan(x) + 37x$')
+plt.xlabel('Eixo x')
+plt.ylabel('Eixo y')
+
+# Adiciona uma grade ao gráfico
+plt.grid(True)
+
+# Adiciona a legenda
+plt.legend()
+
+# Exibe o gráfico
+plt.show()
