609e2a624db47ec35cf8a00915d0a6b3081ef955
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-09 19:40:20 -0300

    Melhorado

--- 500.py
+++ 500.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # A função que representa a equação
 def f(x):
-  return 500 * x + 2
+  return 400 * x + 2
 
 # Cria um intervalo de valores para x.
 # O np.linspace gera 100 pontos entre -10 e 10, o que é um bom intervalo para a maioria dos casos.
@@ -13,10 +13,10 @@ x = np.linspace(-10, 10, 100)
 y = f(x)
 
 # Plota a linha
-plt.plot(x, y, label='y = 500x + 2')
+plt.plot(x, y, label='y = 400x + 2')
 
 # Adiciona títulos e rótulos
-plt.title('Gráfico da Equação y = 500x + 2')
+plt.title('Gráfico da Equação y = 400x + 2')
 plt.xlabel('Eixo X')
 plt.ylabel('Eixo Y')
 
