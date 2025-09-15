b2476bebd74de23bd20faf2819dc0b9509a00828
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-15 20:15:07 -0300

    erro

--- modsenxcosx.py
+++ modsenxcosx.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 
 # Gera valores de x de -2*pi a 2*pi com 1000 pontos para uma curva suave
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+
+y = np.abs(np.sin(x) * np.cos(x))
+
+# Cria a figura e os eixos
+plt.figure(figsize=(10, 6))
+
+# Plota a função
+plt.plot(x, y, label=r'$|sen(x)cos(x)|$')
