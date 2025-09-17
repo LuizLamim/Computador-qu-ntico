3a8d08755a82825d28f132e112d0c36a991b0f43
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-16 21:26:03 -0300

    início

---
+++ aniatangulo.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+# Inicializa o Pygame
+pygame.init()
+
+# Define as cores
+PRETO = (0, 0, 0)
+VERMELHO = (255, 0, 0)
+
+# Configurações da janela
+LARGURA_JANELA = 800
+ALTURA_JANELA = 600
