dffb118fca62fc370c52ece5e340ae42546da749
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-17 22:37:56 -0300

    início

---
+++ testefun.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+#define FILENAME "dados_cos.dat"
+#define X_MIN -10.0
+#define X_MAX 10.0
+#define STEP 0.1
+
+int main() {
+    FILE *fp;
+    double x, y;
