dd8d7516ff667f1844006213baa48480f681dafb
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-19 19:47:38 -0300

    primeiro

---
+++ gravidadeace.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definindo constantes
+G = 6.67430e-11  # Constante gravitacional universal em m^3 kg^-1 s^-2
+M = 5.972e24     # Massa da Terra em kg
+R = 6.371e6      # Raio médio da Terra em m
