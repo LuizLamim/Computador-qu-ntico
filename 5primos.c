23a7eab14287c4d9a28b919319081c40423878b4
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-23 18:50:32 -0300

    primeiro

---
+++ 5primos.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int count = 0; // Contador de números primos encontrados
+    int num = 2;   // Número a ser verificado, começa em 2
+    
+    printf("Os 5 primeiros numeros primos sao:\n");
