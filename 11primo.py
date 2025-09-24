5888544f0bbf88964e51b3c9b16729d51f36f4d4
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-24 19:52:41 -0300

    2

--- 11primo.py
+++ 11primo.py
@@ -8,3 +8,14 @@ def gerar_primos(n):
     primos = []
     # Começa a busca a partir do número 2 (o primeiro primo)
     numero_candidato = 2
+
+    while len(primos) < n:
+        
+        eh_primo = True
+        
+        for divisor in range(2, int(numero_candidato**0.5) + 1):
+            if numero_candidato % divisor == 0:
+                # Se encontrar um divisor, não é primo
+                eh_primo = False
+                # Pula para o próximo número candidato
+                break
