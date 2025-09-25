8deab3fc92e14b495a7443efb289d239fa3f4070
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-25 20:07:35 -0300

    quase

--- 53.py
+++ 53.py
@@ -9,3 +9,13 @@ NUM_ELEMENTOS = 53
 numeros = np.arange(1, NUM_ELEMENTOS + 1)
 
 # 3. Preparar o gráfico
+
+# Cria a figura (janela) e os eixos do gráfico
+fig, ax = plt.subplots()
+
+# Plota os números. 
+# Usamos 'numeros' tanto para o eixo X quanto para o eixo Y
+# para que o gráfico plote os pontos (1,1), (2,2), ..., (53,53).
+# 'b-' é para uma linha azul contínua (Blue line)
+# 'o' é para adicionar um círculo em cada ponto (Optional marker)
+ax.plot(numeros, numeros, 'bo-', label='Números de 1 a 53')
