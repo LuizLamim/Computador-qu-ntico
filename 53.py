bb24225ae00a7efd4cf93718be2fee8e9b8d70ac
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-25 20:07:18 -0300

    começo

---
+++ 53.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1. Definir quantos números você quer (neste caso, 53)
+NUM_ELEMENTOS = 53
+
+# 2. Gerar os 53 primeiros números inteiros (de 1 a 53)
+# O np.arange(1, NUM_ELEMENTOS + 1) cria um array [1, 2, ..., 53]
+numeros = np.arange(1, NUM_ELEMENTOS + 1)
+
+# 3. Preparar o gráfico
