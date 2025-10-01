6eb4d7fb5c6e408677a51a8bb7ca8c95185ae8da
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-30 21:12:49 -0300

    2

---
+++ soma2n.py
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração das variáveis para os dois números e o resultado
+    int numero1, numero2, soma;
+
+    // Solicita o primeiro número ao usuário
+    printf("Digite o primeiro número inteiro: ");
+    // Lê o primeiro número e armazena na variável numero1
+    scanf("%d", &numero1);
