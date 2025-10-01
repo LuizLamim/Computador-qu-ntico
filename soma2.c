49cde6e17d872665a3ba1cdfb9432a4101995513
LuizLamim <luiz.britohenrique@hotmail.com>
2025-09-30 21:14:01 -0300

    Era c

---
+++ soma2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração das variáveis para os dois números e o resultado
+    int numero1, numero2, soma;
+
+    // Solicita o primeiro número ao usuário
+    printf("Digite o primeiro número inteiro: ");
+    // Lê o primeiro número e armazena na variável numero1
+    scanf("%d", &numero1);
+
+    // Solicita o segundo número ao usuário
+    printf("Digite o segundo número inteiro: ");
+    // Lê o segundo número e armazena na variável numero2
+    scanf("%d", &numero2);
+
+    // Realiza a soma
+    soma = numero1 + numero2;
+
+    // Exibe o resultado da soma
+    printf("\nA soma de %d e %d é: %d\n", numero1, numero2, soma);
+
+    // Retorna 0 para indicar que o programa foi executado com sucesso
+    return 0;
+}
