6082929691323120778b3ca756ea8dd586f98580
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-02 19:52:28 -0300

    teste

--- 3primo.c
+++ 3primo.c
@@ -15,3 +15,20 @@ bool eh_primo(int num) {
     return true;
 }
 
+int main() {
+    int contador = 0;
+    int numero_atual = 2;
+    
+    printf("Os 3 primeiros números primos são:\n");
+
+    // Loop para encontrar os 3 primeiros primos
+    while (contador < 3) {
+        if (eh_primo(numero_atual)) {
+            printf("%d ", numero_atual);
+            contador++;
+        }
+        // Incrementamos e passamos para o próximo número
+        numero_atual++;
+    }
+    
+  
