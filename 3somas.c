c40b5644a996ba1d7c28b244651f76fff3daa264
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-04 20:19:15 -0300

    inicial

---
+++ 3somas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração das variáveis para armazenar os três números e a soma
+    int numero1, numero2, numero3, soma;
+
+    // Solicita ao usuário que insira o primeiro número
+    printf("Digite o primeiro numero inteiro: ");
+    // Lê o primeiro número digitado e armazena em 'numero1'
+    scanf("%d", &numero1);
+
+    // Solicita ao usuário que insira o segundo número
+    printf("Digite o segundo numero inteiro: ");
+    // Lê o segundo número digitado e armazena em 'numero2'
+    scanf("%d", &numero2);
