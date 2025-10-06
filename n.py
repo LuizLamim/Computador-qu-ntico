15f9675abb72631f6600d417f2037d837577977f
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-06 19:30:26 -0300

    teste

--- n.py
+++ n.py
@@ -5,3 +5,6 @@ numero = float(input("Digite um número para ser elevado ao quadrado: "))
 # Calcula o quadrado do número usando o operador '**'
 # O operador '** 2' significa "elevado à potência de 2"
 quadrado = numero ** 2
+
+# Imprime o resultado formatado
+print(f"O quadrado de {numero} é {quadrado}")
