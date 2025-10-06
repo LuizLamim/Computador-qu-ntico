15b30e95ca2e80878015756954c58416c3d0957d
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-06 19:30:15 -0300

    2

--- n.py
+++ n.py
@@ -1,3 +1,7 @@
 # Pede ao usuário para digitar um número
 # A função 'input()' sempre retorna uma string, então usamos 'float()' para converter para número
 numero = float(input("Digite um número para ser elevado ao quadrado: "))
+
+# Calcula o quadrado do número usando o operador '**'
+# O operador '** 2' significa "elevado à potência de 2"
+quadrado = numero ** 2
