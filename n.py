3344d499cfed1b9b5cfe73840fc57ab87e863a8c
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-06 19:29:59 -0300

    início

---
+++ n.py
@@ -0,0 +1,3 @@
+# Pede ao usuário para digitar um número
+# A função 'input()' sempre retorna uma string, então usamos 'float()' para converter para número
+numero = float(input("Digite um número para ser elevado ao quadrado: "))
