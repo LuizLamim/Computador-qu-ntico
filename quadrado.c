87055ea05c6a4dbf17a47cf5a3fbf590c998e651
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-08 20:18:02 -0300

    parte 1

---
+++ quadrado.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Função principal do programa
+int main() {
+    // Declaração de variáveis para armazenar os dois números
+    int numero1;
+    int numero2;
+
+    // --- Entrada de Dados ---
+
+    // Solicita o primeiro número
+    printf("Digite o primeiro número inteiro: ");
+    // Lê o primeiro número digitado pelo usuário
+    scanf("%d", &numero1);
+
+    // Solicita o segundo número
+    printf("Digite o segundo número inteiro: ");
+    // Lê o segundo número digitado pelo usuário
+    scanf("%d", &numero2);
