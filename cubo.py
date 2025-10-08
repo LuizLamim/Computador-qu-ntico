864c569f3da82fbb5457838c64db9a8c7df94dc8
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-07 22:41:25 -0300

    quase

--- cubo.py
+++ cubo.py
@@ -1,3 +1,10 @@
 def elevar_ao_cubo_exp(numero):
 
     return numero ** 3
+try:
+  entrada_usuario = float(input("Digite um número para elevar ao cubo: "))
+except ValueError:
+  print("Entrada inválida. Por favor, digite um número.")
+else:
+  resultado = elevar_ao_cubo_exp(entrada_usuario)
+  print(f"O número {entrada_usuario} elevado ao cubo é: {resultado}")
