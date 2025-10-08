edc8b1e2cfbfb20a7061d9d62939d2e6b327ed4c
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-08 20:18:21 -0300

    parte 2

--- quadrado.c
+++ quadrado.c
@@ -17,3 +17,13 @@ int main() {
     printf("Digite o segundo número inteiro: ");
     // Lê o segundo número digitado pelo usuário
     scanf("%d", &numero2);
+
+    // --- Processamento e Saída ---
+
+    // Calcula o quadrado do primeiro número (numero1 * numero1)
+    // e exibe o resultado
+    printf("\nO quadrado de %d é: %d\n", numero1, numero1 * numero1);
+
+    // Calcula o quadrado do segundo número (numero2 * numero2)
+    // e exibe o resultado
+    printf("O quadrado de %d é: %d\n", numero2, numero2 * numero2);
