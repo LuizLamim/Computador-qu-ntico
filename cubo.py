0a9d93c29de8ce8ea09d9386d2dc4e4c2a00c8e1
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-07 22:41:48 -0300

    pronto

--- cubo.py
+++ cubo.py
@@ -8,3 +8,7 @@ except ValueError:
 else:
   resultado = elevar_ao_cubo_exp(entrada_usuario)
   print(f"O número {entrada_usuario} elevado ao cubo é: {resultado}")
+
+print(f"5 elevado ao cubo é: {elevar_ao_cubo_exp(5)}")    # Saída: 125
+print(f"2.5 elevado ao cubo é: {elevar_ao_cubo_exp(2.5)}") # Saída: 15.625
+print(f"-3 elevado ao cubo é: {elevar_ao_cubo_exp(-3)}")  # Saída: -27
