679e8eafe2fc79fa86c37328034b4fd70aa06b12
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-09 18:38:43 -0300

    parte 1

---
+++ multiplica2n.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Declara as variáveis para armazenar os dois números e o resultado
+    int num1, num2, resultado;
+
+    // Pede ao usuário para digitar o primeiro número
+    printf("Digite o primeiro número: ");
+    // Lê o primeiro número digitado e armazena em num1
+    scanf("%d", &num1);
+
+    // Pede ao usuário para digitar o segundo número
+    printf("Digite o segundo número: ");
+    // Lê o segundo número digitado e armazena em num2
+    scanf("%d", &num2);
