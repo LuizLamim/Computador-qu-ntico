426f92118c3b47db4e6d59dd423e101275a37007
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-16 19:45:20 -0300

    meio

--- modulofunc3x.py
+++ modulofunc3x.py
@@ -12,3 +12,8 @@ plt.plot(x, y, label=r'$f(x) = |-3x + 4|$', color='blue')
 plt.title('Gráfico da Função Modular $f(x) = |-3x + 4|$', fontsize=16)
 plt.xlabel('Eixo x', fontsize=12)
 plt.ylabel('Eixo y', fontsize=12)
+
+plt.grid(True, linestyle='--', alpha=0.7)
+
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
