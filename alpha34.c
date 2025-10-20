fd71d3af2e3e29662ab9cb44036a0894f8b269f3
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-20 19:38:31 -0300

    meio

--- alpha34.c
+++ alpha34.c
@@ -3,3 +3,10 @@
 int main() {
     // Declara um caractere inicial
     char letra = 'A';
+
+    for (int i = 0; i < 5; i++) {
+        // Imprime o caractere atual
+        printf("%c ", letra);
+
+        letra = letra + 1;
+    }
