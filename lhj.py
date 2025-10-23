4d542c01b052784efac5e60353b82008668872b0
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-22 23:19:22 -0300

    meio

--- lhj.py
+++ lhj.py
@@ -7,3 +7,14 @@ def eh_primo(numero):
     if numero % 2 == 0 or numero % 3 == 0:
         return False
     i = 5
+    while i * i <= numero:
+        if numero % i == 0 or numero % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def encontrar_e_imprimir_primos(quantidade):
+    """Encontra e imprime os primeiros 'quantidade' números primos."""
+    primos_encontrados = 0
+    numero_atual = 2  # O primeiro número primo é 2
+    lista_primos = []
