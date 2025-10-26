35ed0a59e2590e77f9df5f6b512c73ce2127622e
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-25 22:21:12 -0300

    teste

--- teste.js
+++ teste.js
@@ -6,3 +6,8 @@ const a = 5;
 const b = 10;
 
 const resultado = somarNumeros(a, b);
+
+console.log("O primeiro número é:", a);
+console.log("O segundo número é:", b);
+console.log("A soma dos dois números é:", resultado);
+
