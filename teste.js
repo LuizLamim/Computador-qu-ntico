9722c92f0d541c3ff3c7503eb1ba4e396f46db12
LuizLamim <luiz.britohenrique@hotmail.com>
2025-10-25 22:21:20 -0300

    correto

--- teste.js
+++ teste.js
@@ -11,3 +11,9 @@ console.log("O primeiro número é:", a);
 console.log("O segundo número é:", b);
 console.log("A soma dos dois números é:", resultado);
 
+const x = 25.5;
+const y = 14.5;
+const resultado2 = somarNumeros(x, y);
+
+console.log("\n--- Outro Exemplo ---");
+console.log("A soma de", x, "e", y, "é:", resultado2);
