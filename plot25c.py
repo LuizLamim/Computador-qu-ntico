2b089518d866a60227ad82d3e19c8d8c26ba6898
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-01 17:22:34 -0300

    meio

--- plot25c.py
+++ plot25c.py
@@ -7,3 +7,13 @@ y = numeros
 
 plt.figure(figsize=(10, 6)) 
 
+plt.plot(x, y, 
+         marker='o',          
+         linestyle='-',       
+         color='blue',        
+         label='Primeiros 25 Números') 
+
+plt.title('Gráfico dos 25 Primeiros Números Inteiros', fontsize=16)
+plt.xlabel('Ordem do Número (Eixo X)', fontsize=12)
+plt.ylabel('Valor do Número (Eixo Y)', fontsize=12)
+
