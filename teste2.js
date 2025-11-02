ebe7482918652d963a7a34024a7f56f9649c541b
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-02 20:05:33 -0300

    erro

---
+++ teste2.js
@@ -0,0 +1,5 @@
+function imprimirDezPrimeirosPositivos() {
+    // Definimos o limite (quantos números queremos imprimir)
+    const limite = 10;
+
+    console.log("Os 10 primeiros números positivos são:")
