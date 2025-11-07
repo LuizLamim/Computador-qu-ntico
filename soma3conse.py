d757b34cf810a92c088c8d6af5bfb2e4daf2226e
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-07 20:02:35 -0300

    início

---
+++ soma3conse.py
@@ -0,0 +1,8 @@
+def somar_tres_consecutivos(primeiro_numero):
+ 
+    segundo_numero = primeiro_numero + 1
+    terceiro_numero = primeiro_numero + 2
+    soma = primeiro_numero + segundo_numero + terceiro_numero
+
+
+   return soma, primeiro_numero, segundo_numero, terceiro_numero
