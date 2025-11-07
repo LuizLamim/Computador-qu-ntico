8a8378c19ba2406023066c7f52a868f801613a1b
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-07 20:03:08 -0300

    conti

--- soma3conse.py
+++ soma3conse.py
@@ -6,3 +6,10 @@ def somar_tres_consecutivos(primeiro_numero):
 
 
    return soma, primeiro_numero, segundo_numero, terceiro_numero
+
+while True:
+    try:
+        num_inicial = int(input("Digite o primeiro de 3 números consecutivos: "))
+        break  
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número inteiro.")
