d62b23321ba12bb44a6c53edc5d57edb82af7e47
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-09 19:52:37 -0300

    3

--- 2num.js
+++ 2num.js
@@ -2,3 +2,7 @@ const numero1 = 10;
 const numero2 = 5;
 
 const resultado = numero1 + numero2;
+
+console.log("O primeiro número é:", numero1);
+console.log("O segundo número é:", numero2);
+console.log("O resultado da soma é:", **resultado**);
