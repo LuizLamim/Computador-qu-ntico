42419c641651e464751fd1af1ebe913eaa3e23e0
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-13 20:51:12 -0300

    teste

--- matrinula.c
+++ matrinula.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main() {}
+int main() {
+    int matriz_nula[3][3] = {0};
+
+    int linhas = 3;
+    int colunas = 3;
+
+    printf("Matriz Nula 3x3:\n");
+    
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz_nula[i][j]);
+        }
+        printf("\n");
+}
