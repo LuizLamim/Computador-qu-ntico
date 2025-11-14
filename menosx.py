d88565c29b52b4fafbfc471febf7fb9d753a7d06
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-14 20:24:51 -0300

    erro

--- menosx.py
+++ menosx.py
@@ -7,3 +7,11 @@ y = -x
 
 plt.figure(figsize=(8, 6)) 
 
+plt.plot(x, y, label=r'$f(x) = -x$', color='blue', linewidth=2) 
+
+plt.grid(True, linestyle='--', alpha=0.6)
+
+plt.title('Gráfico da Função f(x) = -x', fontsize=16)
+plt.xlabel('Eixo X', fontsize=12)
+plt.ylabel('Eixo Y / f(x)', fontsize=12)
+
