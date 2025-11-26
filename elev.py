2064b7604a2aaa926724439477b89e0956125bd9
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-26 20:16:16 -0300

    2

--- elev.py
+++ elev.py
@@ -1,4 +1,12 @@
 def calcular_potencia():
     print("--- Calculadora de Potência ---")
     
+    try:
+        base = float(input("Digite o número base: "))
+        expoente = float(input("Digite o expoente (para elevar): "))
+
+        resultado = base ** expoente
+
+        print(f"\nResultado: {base} elevado a {expoente} é igual a {resultado}")
+        
     
