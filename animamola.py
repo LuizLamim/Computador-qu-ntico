e062ff175c2d5a2c849d7347ba675a4e635ecf63
LuizLamim <luiz.britohenrique@hotmail.com>
2025-11-25 23:42:14 -0300

    2

--- animamola.py
+++ animamola.py
@@ -26,3 +26,16 @@ def get_spring_coords(start_x, start_y, end_x, end_y, nodes=15, width=0.5):
     x_data += offsets
     
     return x_data, y_data
+
+# --- 3. Configuração do Gráfico ---
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlim(-3, 3)
+ax.set_ylim(-6, 1)
+ax.set_aspect('equal')
+ax.grid(True, linestyle='--', alpha=0.6)
+ax.set_title("Simulação: Oscilador Massa-Mola")
+
+# Elementos gráficos (Mola, Massa e Teto)
+line_spring, = ax.plot([], [], 'k-', lw=2)  # Linha da mola (preta)
+point_mass, = ax.plot([], [], 'ro', markersize=25, markeredgecolor='black') # Massa (bola vermelha)
+ax.plot([-2, 2], [0, 0], 'k-', lw=5) # Teto fixo
