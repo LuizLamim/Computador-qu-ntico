def19659a818140654f1acc5db7b2c5857d687cc
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-01 19:38:04 -0300

    4

--- num3.py
+++ num3.py
@@ -5,3 +5,5 @@ num3 = float(input("Digite o terceiro número: "))
 lista_numeros = [num1, num2, num3]
 
 lista_numeros.sort(reverse=True)
+
+print(f"Os dois maiores números são: {lista_numeros[0]} e {lista_numeros[1]}")
