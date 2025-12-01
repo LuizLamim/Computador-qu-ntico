923367ee710f4922eb022267cd9454a6eda2053d
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-01 19:37:36 -0300

    2

--- num3.py
+++ num3.py
@@ -1,3 +1,5 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 num3 = float(input("Digite o terceiro número: "))
+
+lista_numeros = [num1, num2, num3]
