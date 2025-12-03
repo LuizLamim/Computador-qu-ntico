332b50e55939b14c97c6905df21a8df957bd3bbc
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-03 18:49:41 -0300

    erro

--- animaquedpot.py
+++ animaquedpot.py
@@ -28,3 +28,40 @@ point, = ax.plot([], [], 'ro')
 # Texto mostrando o valor atual
 text_value = ax.text(0.7, 0.8, '', transform=ax.transAxes, 
                      bbox=dict(facecolor='white', alpha=0.5))
+
+ax.legend()
+
+# --- 3. Funções de Animação ---
+
+def init():
+    """Inicializa o fundo da animação (quadro vazio)."""
+    line.set_data([], [])
+    point.set_data([], [])
+    text_value.set_text('')
+    return line, point, text_value
+
+def update(frame):
+    """Atualiza o gráfico a cada frame."""
+    # frame é o índice atual do array de tempo
+    x_data = t[:frame]
+    y_data = potencia[:frame]
+    
+    # Atualiza a linha com todo o histórico até agora
+    line.set_data(x_data, y_data)
+    
+    # Atualiza o ponto apenas na posição atual
+    if len(x_data) > 0:
+        point.set_data([t[frame-1]], [potencia[frame-1]])
+        text_value.set_text(f'Potência: {potencia[frame-1]:.2f} W')
+    
+    return line, point, text_value
+
+ani = FuncAnimation(fig, update, frames=len(t), 
+                    init_func=init, blit=True, interval=30, repeat=False)
+
+
+
+[Image of exponential decay graph]
+
+
+plt.show()
