b63e25277e9bb88937e0f63b4709e59eab1e8b24
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-03 18:48:39 -0300

    3

--- animaquedpot.py
+++ animaquedpot.py
@@ -5,3 +5,8 @@ from matplotlib.animation import FuncAnimation
 t_max = 10.0
 dt = 0.05
 t = np.arange(0, t_max, dt)
+
+P0 = 100.0   # Potência Inicial (ex: 100 Watts ou %)
+k = 0.5
+
+potencia = P0 * np.exp(-k * t)
