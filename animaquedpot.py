4eaaeeceabbe91a37fe6802a809c6656600a7411
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-03 18:48:52 -0300

    4

--- animaquedpot.py
+++ animaquedpot.py
@@ -10,3 +10,7 @@ P0 = 100.0   # Potência Inicial (ex: 100 Watts ou %)
 k = 0.5
 
 potencia = P0 * np.exp(-k * t)
+
+fig, ax = plt.subplots(figsize=(8, 5))
+ax.set_xlim(0, t_max)
+ax.set_ylim(0, P0 + 10)
