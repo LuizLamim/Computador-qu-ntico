5b5dfcf227117000f587f992fee29e42b8157948
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-06 15:47:30 -0300

    3

--- pow.c
+++ pow.c
@@ -6,3 +6,10 @@ int main() {
 
     printf("Digite um numero para elevar a 10a potencia: ");
     scanf("%lf", &numero);
+
+    resultado = pow(numero, 10);
+
+    printf("O numero %.2lf elevado a 10 e: %.2lf\n", numero, resultado);
+
+    return 0;
+}
