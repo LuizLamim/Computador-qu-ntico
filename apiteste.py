54869c1318e57055166454f99a6ffcf02c2f6fa1
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-10 21:36:43 -0300

    3

--- apiteste.py
+++ apiteste.py
@@ -11,3 +11,15 @@ class Produto(BaseModel):
     @app.get("/")
 def home():
     return {"mensagem": "API rodando com sucesso no VS Code!"}
+
+@app.get("/produtos/{item_id}")
+def ler_item(item_id: int):
+    return {"item_id": item_id, "nome": "Item de Exemplo"}
+
+@app.post("/produtos/")
+def criar_produto(produto: Produto):
+    return {
+        "status": "Produto criado",
+        "produto_recebido": produto.nome,
+        "valor": produto.preco
+    }
