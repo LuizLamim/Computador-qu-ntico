729d86fb06244611f7e8afb2a70052f2cd466b85
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-11 22:22:54 -0300

    3

--- perceptronhoje.py
+++ perceptronhoje.py
@@ -8,4 +8,35 @@ class Perceptron:
         self.weights = None
         self.bias = None
 
-        
+    def _unit_step_func(self, x):
+        """Função de ativação Degrau (Heaviside step function)"""
+        return np.where(x >= 0, 1, 0)
+
+    def fit(self, X, y):
+        """
+        Treina o modelo com os dados.
+        :param X: Matriz de entrada (amostras x características)
+        :param y: Vetor de alvos (labels verdadeiros)
+        """
+        n_samples, n_features = X.shape
+
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                
+                # Calcular a saída linear: z = w * x + b
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                
+                # Aplicar função de ativação
+                y_predicted = self.activation_func(linear_output)
+
+                # Regra de atualização do Perceptron:
+                # w = w + taxa * (real - previsto) * entrada
+                update = self.lr * (y[idx] - y_predicted)
+                
+                self.weights += update * x_i
+                self.bias += update
+
+                
