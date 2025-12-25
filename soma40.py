145c5892301365a486587f6d749610f55c755d22
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-25 19:00:43 -0300

    2

--- soma40.py
+++ soma40.py
@@ -1,5 +1,14 @@
 # A função range(1, 41) gera números de 1 até 40
 # (o último número, 41, é exclusivo/não entra)
-resultado = sum(range(1, 41))
+#resultado = sum(range(1, 41))
 
-print(f"A soma dos 40 primeiros números é: {resultado}")
+#print(f"A soma dos 40 primeiros números é: {resultado}")
+
+soma = 0
+
+# Para cada número 'i' no intervalo de 1 a 40
+for i in range(1, 41):
+    soma += i  # Adiciona o valor de 'i' à variável 'soma'
+    # É o mesmo que fazer: soma = soma + i
+
+print(f"A soma é: {soma}")
