67e1ffec3d6382ab3c72a91dff9bb1a17e4359e3
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-25 19:00:16 -0300

    1

---
+++ soma40.py
@@ -0,0 +1,5 @@
+# A função range(1, 41) gera números de 1 até 40
+# (o último número, 41, é exclusivo/não entra)
+resultado = sum(range(1, 41))
+
+print(f"A soma dos 40 primeiros números é: {resultado}")
