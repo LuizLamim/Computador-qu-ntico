266734bf2522324e1009f3bee91b83fe80bbbb1b
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-26 21:19:05 -0300

    Exemplo 2

--- pythonlacos.py
+++ pythonlacos.py
@@ -2,3 +2,7 @@ frutas = ["Maçã", "Banana", "Cereja"]
 
 for fruta in frutas:
     print(f"Eu gosto de {fruta}")
+
+    # O range(5) gera números de 0 a 4
+for i in range(5):
+    print(f"Contagem: {i}")
