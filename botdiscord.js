17dfdbac8b6ad8eadacd37c117f3c1993f31eb60
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-28 20:35:49 -0300

    seguindo

--- botdiscord.js
+++ botdiscord.js
@@ -1,2 +1,10 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,           // Para interagir com servidores
+        GatewayIntentBits.GuildMessages,    // Para ver mensagens
+        GatewayIntentBits.MessageContent    // Para ler o conteúdo das mensagens
+    ]
+});
