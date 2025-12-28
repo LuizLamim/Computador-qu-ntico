0a44546df514282b13a9239eab999e5d40bca896
LuizLamim <luiz.britohenrique@hotmail.com>
2025-12-27 21:52:30 -0300

    2

--- hj.c
+++ hj.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+int main() {
+    // 1. Declaração das variáveis
+    int numero1, numero2, resultado;
+
+    // 2. Solicita o primeiro número
+    printf("Digite o primeiro número: ");
+    scanf("%d", &numero1);
+
+    // 3. Solicita o segundo número
+    printf("Digite o segundo número: ");
+    scanf("%d", &numero2);
