f09fa00c3e154ce8718cfd1643d267af78f31e07
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-04 20:57:07 -0300

    pronto

--- sen10cos.py
+++ sen10cos.py
@@ -7,3 +7,13 @@ y = np.sin(x) + 10 * np.cos(x)
 
 plt.figure(figsize=(10, 6)) # Define o tamanho da figura
 plt.plot(x, y, label=r'$\sin(x) + 10\cos(x)$', color='blue')
+
+plt.title('Gráfico da Função: sen(x) + 10cos(x)')
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+plt.axhline(0, color='black', linewidth=0.8) # Linha do eixo X
+plt.axvline(0, color='black', linewidth=0.8) # Linha do eixo Y
+plt.grid(True, linestyle='--', alpha=0.6)    # Grade de fundo
+plt.legend()
+
+plt.show()
