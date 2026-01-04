4e716a19c08a0bb283af57ddb5c656b08691716f
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-04 20:56:48 -0300

    gráfico

--- sen10cos.py
+++ sen10cos.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 x = np.linspace(-4 * np.pi, 4 * np.pi, 1000)
 
 y = np.sin(x) + 10 * np.cos(x)
+
+plt.figure(figsize=(10, 6)) # Define o tamanho da figura
+plt.plot(x, y, label=r'$\sin(x) + 10\cos(x)$', color='blue')
