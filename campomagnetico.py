458ea8d0a942b7d6ad7bcdbaae14dbccb8453cfe
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-05 22:45:42 -0300

    Componetes

--- campomagnetico.py
+++ campomagnetico.py
@@ -6,3 +6,14 @@ def plot_magnetic_field():
     x = np.linspace(-2, 2, nx)
     y = np.linspace(-2, 2, ny)
     X, Y = np.meshgrid(x, y)
+
+    m_vector = np.array([0, 1])
+    
+    R[R == 0] = 1e-10
+
+    dot_mr = m_vector[0] * X + m_vector[1] * Y
+    
+    constante = 1.0
+
+    vec_term_x = 3 * dot_mr * X
+    vec_term_y = 3 * dot_mr * Y
