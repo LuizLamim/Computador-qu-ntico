975848c72a36a519506b575091d9fcdb616ba5a9
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-05 22:44:46 -0300

    Criamos uma matriz de pontos X e Y variando de -2 a 2

--- campomagnetico.py
+++ campomagnetico.py
@@ -1,2 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def plot_magnetic_field():
+    nx, ny = 100, 100
+    x = np.linspace(-2, 2, nx)
+    y = np.linspace(-2, 2, ny)
+    X, Y = np.meshgrid(x, y)
