2e1bea6cf9c2c6031a9ad3f7c099819e1c369175
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-06 20:13:25 -0300

    Variáveis

--- tgx.c
+++ tgx.c
@@ -4,3 +4,13 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+
+int main() {
+    // Configurações do gráfico
+    int largura = 60;        // Largura do terminal em caracteres
+    int centro = largura / 2; 
+    double passo = 0.1;      // Resolução do eixo X
+    double escala_y = 10.0;  // Quão "esticada" a curva será horizontalmente
+
+    printf("Gráfico de tg(x) (Eixo X vertical, Eixo Y horizontal)\n");
+    printf("----------------------------------------------------\n");
