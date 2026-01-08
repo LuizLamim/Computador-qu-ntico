ad58a776dc5285527634da5f64ab04f905e688f8
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-08 19:43:30 -0300

    3

--- aprender.swift
+++ aprender.swift
@@ -1,3 +1,9 @@
 var numeros = [
-    
+    "primes": [2, 3, 5, 7, 11, 13, 17],
+    "triangular": [1, 3, 6, 10, 15, 21, 28],
+    "hexagonal": [1, 6, 15, 28, 45, 66, 91]
 ]
+
+for key in numeros.keys {
+    numeros[key]?.sort(by: >)
+}
