ff0e9c6600cb868bff719d0d6b2509f748c86a87
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-09 22:43:58 -0300

    1

---
+++ exemplo.js
@@ -0,0 +1,8 @@
+let frutas = ["Maçã", "Banana", "Laranja", "Uva"];
+
+// A partir do índice 1 (Banana), remova 2 elementos
+frutas.splice(1, 2); 
+
+console.log(frutas); 
+// Resultado: ["Maçã", "Uva"] -> (Banana e Laranja sumiram)
+
