5bab1d0804a84c6465b0ce1570e0e8a8e39ea981
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-10 20:11:57 -0300

    3

--- primeirospasos.sql
+++ primeirospasos.sql
@@ -3,3 +3,7 @@ SELECT * FROM Customers;
 SELECT   extracts data from a database
 
 UPDATE - updates data in a database 
+
+DELETE - deletes data from a database
+
+INSERT INTO - inserts new data into a database
