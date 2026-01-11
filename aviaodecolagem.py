760b67e260f094b97e69d9cfa06dc61dbc201d7e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-11 20:32:17 -0300

    Configuração da Simulação

--- aviaodecolagem.py
+++ aviaodecolagem.py
@@ -9,3 +9,8 @@ Cd = 0.03          # Coeficiente de arrasto
 Cl = 0.5           # Coeficiente de sustentação (durante a corrida)
 mu = 0.02          # Coeficiente de atrito de rolamento
 g = 9.81           # Gravidade (m/s^2)
+
+dt = 0.1           # Intervalo de tempo (segundos)
+v = 0.0            # Velocidade inicial
+x = 0.0            # Posição inicial
+v_decolagem = 80   # Velocidade de rotação (m/s) (~290 km/h)
