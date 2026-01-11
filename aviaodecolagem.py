29e73a003f8f125d389a278ab324c29b8eb4014b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-11 20:31:55 -0300

    Parâmetros Simulados (Baseados em um jato comercial médio)

--- aviaodecolagem.py
+++ aviaodecolagem.py
@@ -1,2 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+m = 70000.0        # Massa (kg)
+thrust = 240000.0  # Empuxo total dos motores (N)
+rho = 1.225        # Densidade do ar (kg/m^3)
+S = 125.0          # Área das asas (m^2)
+Cd = 0.03          # Coeficiente de arrasto
+Cl = 0.5           # Coeficiente de sustentação (durante a corrida)
+mu = 0.02          # Coeficiente de atrito de rolamento
+g = 9.81           # Gravidade (m/s^2)
