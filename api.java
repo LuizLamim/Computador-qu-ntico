af4b27a2fb3561fdae5a59b8de80a46dcdeb4183
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-12 21:02:38 -0300

    2

--- api.java
+++ api.java
@@ -1,1 +1,13 @@
 package com.exemplo.api.model;
+
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String email;
+
+    // Construtores
+    public Usuario(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
