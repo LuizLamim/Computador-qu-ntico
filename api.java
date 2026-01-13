4e8f93deb680795bee4f762b279784d628c35730
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-12 21:53:07 -0300

    3

--- api.java
+++ api.java
@@ -11,3 +11,12 @@ public class Usuario {
         this.nome = nome;
         this.email = email;
     }
+
+    // Getters e Setters (Necessários para o Spring converter para JSON)
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
