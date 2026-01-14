6aafa0d68cd9860b74f6abd3b26d6b22c8f4757b
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-13 22:22:31 -0300

    Cria a tabela de Produtos

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE SistemaEstoque;
 
 USE SistemaEstoque;
+
+CREATE TABLE Produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- Identificador único automático
+    nome VARCHAR(100) NOT NULL,         -- Nome do produto (texto)
+    categoria VARCHAR(50),              -- Categoria (ex: Eletrônicos)
+    preco DECIMAL(10, 2),               -- Preço com 2 casas decimais
+    quantidade INT DEFAULT 0,           -- Quantidade em estoque (número inteiro)
+    data_cadastro DATE                  -- Data que o produto entrou
+);
