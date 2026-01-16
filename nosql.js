abfd61ca5e705556e86e639fe6a46662c2af18d5
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-16 20:50:13 -0300

    4

--- nosql.js
+++ nosql.js
@@ -5,3 +5,11 @@ db.usuarios.insertOne({
   endereco: { rua: "Av. Paulista, 1000", cidade: "São Paulo" },
   interesses: ["tecnologia", "livros"]
 })
+
+db.produtos.insertOne({
+  nome: "Notebook Gamer X",
+  preco: 4500.00,
+  categoria: "Eletrônicos",
+  detalhes: { ram: "16GB", cpu: "i7" },
+  tags: ["computador", "gaming"]
+})
