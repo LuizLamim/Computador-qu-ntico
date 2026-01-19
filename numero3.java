a8100a8cf034371d179e685f072e158a408f9699
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-19 00:23:51 -0300

    3

--- numero3.java
+++ numero3.java
@@ -1,3 +1,22 @@
 
 public class SomaTresNumeros {
     public static void main(String[] args) {
+
+        canner leitor = new Scanner(System.in);
+        
+        System.out.println("--- Calculadora de Soma de 3 Números ---");
+
+        // 2. Pede e lê o primeiro número
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = leitor.nextInt();
+
+        // 3. Pede e lê o segundo número
+        System.out.print("Digite o segundo número: ");
+        int numero2 = leitor.nextInt();
+
+        // 4. Pede e lê o terceiro número
+        System.out.print("Digite o terceiro número: ");
+        int numero3 = leitor.nextInt();
+
+        // 5. Realiza a soma
+        int soma = numero1 + numero2 + numero3;
