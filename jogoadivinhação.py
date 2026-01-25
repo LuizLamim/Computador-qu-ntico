95e12bd677c6b6ed16b885f87ff772d635bd5a21
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-25 17:10:20 -0300

    While

--- jogoadivinhação.py
+++ jogoadivinhação.py
@@ -8,4 +8,17 @@ def jogo_adivinhacao():
     tentativas = 0
     acertou = False
 
-    
+    while not acertou:
+        try:
+            palpite = int(input("Qual é o seu palpite? "))
+            tentativas += 1
+
+            if palpite < numero_secreto:
+                print("Mais alto! Tente novamente.")
+            elif palpite > numero_secreto:
+                print("Mais baixo! Tente novamente.")
+            else:
+                print(f"Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativas!")
+                acertou = True
+        except ValueError:
+            print("Por favor, digite apenas números inteiros.")
