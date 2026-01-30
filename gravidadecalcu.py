cf23cabe2bbcc0ccb72b15148b2d6ee7b67352bd
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-29 23:49:41 -0300

    2

--- gravidadecalcu.py
+++ gravidadecalcu.py
@@ -2,3 +2,10 @@ def calcular_queda_livre():
     print("--- Calculadora de Queda Livre ---")
     print("Este programa calcula a distância percorrida e a velocidade final")
     print("de um objeto caindo no vácuo (ignorando a resistência do ar).\n")
+
+    try:
+        # 1. Definindo a constante da gravidade (Terra)
+        g = 9.81  # m/s^2
+
+        # 2. Recebendo o tempo de queda do usuário
+        tempo = float(input("Digite o tempo de queda (em segundos): "))
