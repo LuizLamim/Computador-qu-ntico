18c16e552fbc2792a52a20df97199eddccb9ee8a
LuizLamim <luiz.britohenrique@hotmail.com>
2026-01-29 23:49:07 -0300

    1

---
+++ gravidadecalcu.py
@@ -0,0 +1,4 @@
+def calcular_queda_livre():
+    print("--- Calculadora de Queda Livre ---")
+    print("Este programa calcula a distância percorrida e a velocidade final")
+    print("de um objeto caindo no vácuo (ignorando a resistência do ar).\n")
