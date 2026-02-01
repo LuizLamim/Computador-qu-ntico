ea9d21343cc4c638de62dc674aad598aa656b4b1
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-01 20:13:42 -0300

    Condicionais

--- FuncoesUsuais.java
+++ FuncoesUsuais.java
@@ -11,3 +11,13 @@ public class FuncoesUsuais {
         System.out.println("--- Bem-vindo ao Demo Java ---");
         System.out.print("Digite seu nome: ");
         String nome = leitor.nextLine();
+
+        // 2. Condicionais (if/else)
+        System.out.print("Digite sua idade: ");
+        int idade = leitor.nextInt();
+
+        if (idade >= 18) {
+            System.out.println(nome + ", você é maior de idade.");
+        } else {
+            System.out.println(nome + ", você é menor de idade.");
+        }
