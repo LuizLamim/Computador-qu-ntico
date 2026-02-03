8c5c465102aa2d615808fef3eba69392e053b37e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-03 11:23:22 -0300

    Exemplo de API básica

--- apiparatestar.py
+++ apiparatestar.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Definimos a rota (endereço) e o método
+# Definimos a rota (endereço) e o método ex
 @app.route('/saudar', methods=['GET'])
 def home():
     dados = {
