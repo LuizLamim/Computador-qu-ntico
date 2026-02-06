5fff44560c729b91990a2ee329bb23142ff18884
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-06 20:57:27 -0300

    Constantes

--- animaeletron.py
+++ animaeletron.py
@@ -2,3 +2,7 @@ from vpython import sphere, vector, color, rate, canvas, arrow
 
 scene = canvas(title='Movimento de um Elétron em Campo Magnético', 
                width=800, height=600, background=color.black)
+
+q = -1.6e-19  # Carga do elétron
+m = 9.1e-31   # Massa do elétron
+B_field = vector(0, 0, 5e-11) # Campo magnético apontando para "fora" da tela (eixo Z)
