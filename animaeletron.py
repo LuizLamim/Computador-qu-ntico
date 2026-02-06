b6bc1c1d6fe423c8fc8c255f69fc136c6aae2bef
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-06 20:57:06 -0300

    Configuração do cenário

--- animaeletron.py
+++ animaeletron.py
@@ -1,1 +1,4 @@
 from vpython import sphere, vector, color, rate, canvas, arrow
+
+scene = canvas(title='Movimento de um Elétron em Campo Magnético', 
+               width=800, height=600, background=color.black)
