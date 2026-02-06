ac350b78c1ffabf6d9d46dfb755f50e152b784c4
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-05 21:38:17 -0300

    3

--- animaesfera.py
+++ animaesfera.py
@@ -3,3 +3,5 @@ import numpy as np
 
 # Configuração da tela
 scene = canvas(title='Animação de Esfera 3D', width=800, height=600, center=vector(0,0,0), background=color.black)
+
+bola = sphere(pos=vector(0,0,0), radius=1, color=color.cyan, make_trail=True)
