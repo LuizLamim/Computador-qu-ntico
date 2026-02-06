54d057ecdcf62e7f3edbab74146e17d4182235f4
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-06 20:59:01 -0300

    Quase

--- animaeletron.py
+++ animaeletron.py
@@ -13,3 +13,18 @@ eletron.v = vector(2, 1, 0) # Velocidade inicial (componente em Y gera o movimen
 for x in range(-5, 6, 2):
     for y in range(-3, 4, 2):
         arrow(pos=vector(x, y, 0), axis=vector(0, 0, 0.5), color=color.blue, shaftwidth=0.05)
+
+dt = 0.01
+
+while True:
+    rate(100)
+    
+    # Cálculo da Força de Lorentz: F = q * (v produto vetorial B)
+    F_lorentz = q * cross(eletron.v, B_field)
+    
+    # Segunda Lei de Newton: a = F / m
+    aceleracao = F_lorentz / m
+    
+    # Atualização da velocidade e posição (Método de Euler)
+    eletron.v = eletron.v + aceleracao * dt
+    eletron.pos = eletron.pos + eletron.v * dt
