ad71e0131b18fd26100c874256b3c4a6d8dcd188
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-05 21:37:59 -0300

    2

--- animaesfera.py
+++ animaesfera.py
@@ -1,2 +1,5 @@
 from vpython import sphere, vector, color, rate, canvas
 import numpy as np
+
+# Configuração da tela
+scene = canvas(title='Animação de Esfera 3D', width=800, height=600, center=vector(0,0,0), background=color.black)
