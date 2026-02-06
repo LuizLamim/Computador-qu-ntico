f5368f6ee235dff876e9b6cf96b841b071c8f85d
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-06 20:58:20 -0300

    Campo

--- animaeletron.py
+++ animaeletron.py
@@ -9,3 +9,7 @@ B_field = vector(0, 0, 5e-11) # Campo magnético apontando para "fora" da tela (
 
 eletron = sphere(pos=vector(-5, 0, 0), radius=0.2, color=color.yellow, make_trail=True)
 eletron.v = vector(2, 1, 0) # Velocidade inicial (componente em Y gera o movimento helicoidal)
+
+for x in range(-5, 6, 2):
+    for y in range(-3, 4, 2):
+        arrow(pos=vector(x, y, 0), axis=vector(0, 0, 0.5), color=color.blue, shaftwidth=0.05)
