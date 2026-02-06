bb1abf7638e7b7520e2812d0b11b0c60c9171ca1
LuizLamim <luiz.britohenrique@hotmail.com>
2026-02-06 20:57:41 -0300

    Criando elétrons

--- animaeletron.py
+++ animaeletron.py
@@ -6,3 +6,6 @@ scene = canvas(title='Movimento de um Elétron em Campo Magnético',
 q = -1.6e-19  # Carga do elétron
 m = 9.1e-31   # Massa do elétron
 B_field = vector(0, 0, 5e-11) # Campo magnético apontando para "fora" da tela (eixo Z)
+
+eletron = sphere(pos=vector(-5, 0, 0), radius=0.2, color=color.yellow, make_trail=True)
+eletron.v = vector(2, 1, 0) # Velocidade inicial (componente em Y gera o movimento helicoidal)
